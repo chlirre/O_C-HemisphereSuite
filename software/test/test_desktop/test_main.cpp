46b4a3614e2bb93b34b265016e37bc8fecda2051
chlirre <christoffer@chlirre.com>
2020-12-20 22:37:34 +0100

    Restructure tests and add S&H tests.
    Restrucuture allows for multiple test files.

---
+++ software/test/test_desktop/test_main.cpp
@@ -0,0 +1,25 @@
+#include <unity.h> 
+#include "unittest_turing_machine.h"
+#include "unittest_sample_and_hold.h"
+
+int main(int argc, char **argv) {
+    UNITY_BEGIN();
+
+    // Turing Machine
+    RUN_TEST(test_function_get_register);
+    RUN_TEST(test_function_cycle);
+    RUN_TEST(test_function_length);
+    RUN_TEST(test_function_probability_100);
+    RUN_TEST(test_function_set_probability_50);
+    RUN_TEST(test_function_set_probability_0);
+    RUN_TEST(test_function_probability_0);
+
+    // Sample n Hold
+    RUN_TEST(test_function_get_value);
+    RUN_TEST(test_function_sample_returns_value);
+    RUN_TEST(test_function_get_value_same_until_sample);
+    RUN_TEST(test_function_initial_value_is_sampled);
+
+    UNITY_END();
+    return 0;
+}
---
+++ software/test/test_desktop/unittest_turing_machine.h
@@ -0,0 +1,25 @@
+/*
+ Copyright (c) 2014-present PlatformIO <contact@platformio.org>
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+**/
+#ifndef _TEST_TURING_MACHINE_H_
+#define _TEST_TURING_MACHINE_H_
+
+void test_function_get_register(void);
+void test_function_cycle(void);
+void test_function_length(void);
+void test_function_probability_100(void);
+void test_function_set_probability_50(void);
+void test_function_probability_50(void);
+void test_function_set_probability_0(void);
+void test_function_probability_0(void);
+
+#endif // _TEST_TURING_MACHINE_H_
